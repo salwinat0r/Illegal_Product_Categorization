fe27906ca3ad8fbf036e4c77c64f84f3856aa4d7
Salwyn <salwynmathew13@gmail.com>
2023-11-06 23:37:40 +0530

    remove key

--- name.py
+++ name.py
@@ -1,6 +1,7 @@
 import cohere
 
-co = cohere.Client("SkHqLMB1VLkJsiyGgghtpK4R3R19Ro49uNMJ2eU8")
+key = open("key.txt", 'r').read().strip('\n')
+co = cohere.Client(key)
 
 prompt = """
 Unique-AR's (AR-15) rifle is custom built on a matched billet receiver set with a 416r stainless steel barrel, precision rifled, and hand lapped to create an attractive lightweight and functional weapon. Chambered in 223 Wylde, this 5r matte finish barrel provides desirable performance and guaranteed moa accuracy. The smooth pull (ELG) trigger, Rook muzzle brake, ambi-bolt release (not available with Sharps Bros. lowers) and skeleton A-frame buttstock to deliver the best weight, reliability, and performance characteristics. Includes a soft rifle case and two Lancer advanced war-fighter 30rd magazines.
