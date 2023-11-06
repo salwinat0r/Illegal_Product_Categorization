14da8b0d33eee26c1d80355ea7ca0600d69ddfe8
Salwyn <salwynmathew13@gmail.com>
2023-11-06 23:33:28 +0530

    prompt engineered

--- name.py
+++ name.py
@@ -1,12 +1,18 @@
-import openai
+import cohere
 
-openai.api_key = "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"
+co = cohere.Client("SkHqLMB1VLkJsiyGgghtpK4R3R19Ro49uNMJ2eU8")
 
-completion = openai.Completion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+prompt = """
+Unique-AR's (AR-15) rifle is custom built on a matched billet receiver set with a 416r stainless steel barrel, precision rifled, and hand lapped to create an attractive lightweight and functional weapon. Chambered in 223 Wylde, this 5r matte finish barrel provides desirable performance and guaranteed moa accuracy. The smooth pull (ELG) trigger, Rook muzzle brake, ambi-bolt release (not available with Sharps Bros. lowers) and skeleton A-frame buttstock to deliver the best weight, reliability, and performance characteristics. Includes a soft rifle case and two Lancer advanced war-fighter 30rd magazines.
 
-print(completion.choices[0].message)
+If the above paragraph is about firearms, return 'Illegal'.
+"""
+
+response = co.generate(  
+    model='command-nightly',  
+    prompt = prompt,  
+    max_tokens=200,  
+    temperature=0.750)
+intro_paragraph = response.generations[0].text
+
+print(intro_paragraph)
